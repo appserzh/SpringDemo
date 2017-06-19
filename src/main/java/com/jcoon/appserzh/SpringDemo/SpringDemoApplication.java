85661f1873e798eb2f29e5fbdf76afd0c952562e
Sergey Luchkov <appserzh@gmail.com>
2017-06-19 14:11:12 +0300

    first commit

---
+++ src/main/java/com/jcoon/appserzh/SpringDemo/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jcoon.appserzh.SpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//CHECKSTYLE DISABLE HideUtilityClassConstructor FOR 1 LINES
+@SpringBootApplication
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+}
